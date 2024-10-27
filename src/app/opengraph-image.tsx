01d0b7d09610c48983d9f25fd8ba5ad7021449be
Julio Rarick <juliorarick@gmail.com>
2024-10-27 15:46:30 -0300

    .

--- src/app/opengraph-image.tsx
+++ src/app/opengraph-image.tsx
@@ -6,7 +6,7 @@ export default async function Image() {
   const logoImage = 'logo-image.png'
   const logoSrc = new URL(
     logoImage,
-    'https://report-formatter.vercel.app/',
+    'https://report-formatter.vercel.app',
   ).toString()
 
   return new ImageResponse(
